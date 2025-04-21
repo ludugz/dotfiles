233b390a845379e3839f868fd8d7aca93ff5e860
Tan N. Truong <tntan92@gmail.com>
2025-04-21 15:45:40 +0700

    Add show reference shortcut, toggle between files & add nightfox theme.

--- init.lua
+++ init.lua
@@ -18,7 +18,6 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-
 -- Enable mouse support
 vim.opt.mouse = "a"
 
@@ -41,6 +40,9 @@ vim.keymap.set("n", "<C-e>", vim.diagnostic.open_float, { noremap = true, silent
 -- Set leader key to SPACE
 vim.g.mapleader = " "
 
+-- Set to faster shortcut for toggling between current & previous file
+vim.keymap.set("n", "<leader>e", "<C-^>", { noremap = true, silent = true })
+
 -- Enable hover on Cursor Hold
 -- vim.o.updatetime = 300
 -- vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
@@ -62,11 +64,25 @@ vim.keymap.set("n", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = false })
 -- Not sure if this works
 vim.opt.completeopt = { "menu", "menuone", "noselect", "preview" }
 
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true })
+
 -- Install plugins
 require("lazy").setup({
   {
     "rose-pine/neovim",
     name = "rose-pine",
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false, -- Load immediately
+    priority = 1000, -- Ensure it loads first
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    name = "nightfox",
+    lazy = false, -- Load immediately
+    priority = 1000, -- Ensure it loads first
   },
   {
     "smjonas/inc-rename.nvim",
@@ -92,6 +108,7 @@ require("lazy").setup({
       require("telescope").setup({})
       vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>o", ":Telescope lsp_document_symbols<CR>", { noremap = true, silent = true })
+      vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, { noremap = true, silent = true })
     end
   },
   {
@@ -99,12 +116,6 @@ require("lazy").setup({
     build = ":TSUpdate",
   },
   -- Catppuccin theme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false, -- Load immediately
-    priority = 1000, -- Ensure it loads first
-  },
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -146,8 +157,9 @@ require("lazy").setup({
 })
 
 -- Nvim colorscheme
-vim.cmd.colorscheme("rose-pine-main")
+-- vim.cmd.colorscheme("rose-pine-moon")
 -- vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("nightfox")
 
 -- Flutter tools setup
 require("flutter-tools").setup{}
@@ -216,6 +228,142 @@ require("catppuccin").setup({
     },
     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
+})
+
+-- Set up nightfox colorscheme
+-- Palettes are the base color defines of a colorscheme.
+-- You can override these palettes for each colorscheme defined by nightfox.
+local palettes = {
+  -- Everything defined under `all` will be applied to each style.
+  all = {
+    -- Each palette defines these colors:
+    --   black, red, green, yellow, blue, magenta, cyan, white, orange, pink
+    --
+    -- These colors have 3 shades: base, bright, and dim
+    --
+    -- Defining just a color defines it's base color
+    red = "#ff0000",
+  },
+  nightfox = {
+    -- A specific style's value will be used over the `all`'s value
+    red = "#c94f6d",
+  },
+  dayfox = {
+    -- Defining multiple shades is done by passing a table
+    blue = { base = "#4d688e", bright = "#4e75aa", dim = "#485e7d" },
+  },
+  nordfox = {
+    -- A palette also defines the following:
+    --   bg0, bg1, bg2, bg3, bg4, fg0, fg1, fg2, fg3, sel0, sel1, comment
+    --
+    -- These are the different foreground and background shades used by the theme.
+    -- The base bg and fg is 1, 0 is normally the dark alternative. The others are
+    -- incrementally lighter versions.
+    bg1 = "#2e3440",
+
+    -- sel is different types of selection colors.
+    sel0 = "#3e4a5b", -- Popup bg, visual selection bg
+    sel1 = "#4f6074", -- Popup sel bg, search bg
+
+    -- comment is the definition of the comment color.
+    comment = "#60728a",
+  },
+}
+
+-- Spec's (specifications) are a mapping of palettes to logical groups that will be
+-- used by the groups. Some examples of the groups that specs map would be:
+--   - syntax groups (functions, types, keywords, ...)
+--   - diagnostic groups (error, warning, info, hints)
+--   - git groups (add, removed, changed)
+--
+-- You can override these just like palettes
+local specs = {
+  -- As with palettes, the values defined under `all` will be applied to every style.
+  all = {
+    syntax = {
+      -- Specs allow you to define a value using either a color or template. If the string does
+      -- start with `#` the string will be used as the path of the palette table. Defining just
+      -- a color uses the base version of that color.
+      keyword = "magenta",
+
+      -- Adding either `.bright` or `.dim` will change the value
+      conditional = "magenta.bright",
+      number = "orange.dim",
+    },
+    git = {
+      -- A color define can also be used
+      changed = "#f4a261",
+    },
+  },
+  nightfox = {
+    syntax = {
+      -- As with palettes, a specific style's value will be used over the `all`'s value.
+      operator = "orange",
+    },
+  },
+}
+
+-- Groups are the highlight group definitions. The keys of this table are the name of the highlight
+-- groups that will be overridden. The value is a table with the following values:
+--   - fg, bg, style, sp, link,
+--
+-- Just like `spec` groups support templates. This time the template is based on a spec object.
+local groups = {
+  -- As with specs and palettes, the values defined under `all` will be applied to every style.
+  all = {
+    -- If `link` is defined it will be applied over any other values defined
+    Whitespace = { link = "Comment" },
+
+    -- Specs are used for the template. Specs have their palette's as a field that can be accessed
+    IncSearch = { bg = "palette.cyan" },
+  },
+  nightfox = {
+    -- As with specs and palettes, a specific style's value will be used over the `all`'s value.
+    PmenuSel = { bg = "#73daca", fg = "bg0" },
+  },
+}
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = false,     -- Disable setting background
+    terminal_colors = true,  -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = false,    -- Non focused panes set to alternative background
+    module_default = true,   -- Default enable value for modules
+    colorblind = {
+      enable = false,        -- Enable colorblind support
+      simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+      severity = {
+        protan = 0,          -- Severity [0,1] for protan (red)
+        deutan = 0,          -- Severity [0,1] for deutan (green)
+        tritan = 0,          -- Severity [0,1] for tritan (blue)
+      },
+    },
+    styles = {               -- Style to be applied to different syntax groups
+      comments = "NONE",     -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "NONE",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "NONE",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = palettes,
+  specs = specs,
+  groups = groups,
 })
 
 -- Configure Completion
